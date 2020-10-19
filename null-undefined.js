a5e42e05775569406968f31dada7abef38a93593
BRISTYprogramminghero <71221579+BRISTYprogramminghero@users.noreply.github.com>
2020-10-18 21:13:16 -0700

    null-undefimed

---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1+num2);
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+
+const premik = { name:'hridoy', phone:'o130894'}
+console.log(premik.wife);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 8, 0, ]
+console.log(ages[11])
--- true-false.js
+++ true-false.js
@@ -1,5 +1,19 @@
 const age = 0;
 
+
+// false value;
+// 0
+// ""
+// undefined
+// null
+// NaN
+// FALSE
+
+// true
+// '0',' ', [], {}
+
+
+
 if(age>0){
     console.log("consdition is true");
 }
